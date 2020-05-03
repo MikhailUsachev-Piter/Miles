d4e81b9f3abb1328b79a846aef2910b18f908ee2
MikhailUsachev-Piter <mikhail.usachev@gmail.com>
2020-05-03 15:48:39 +0300

    Added Main.java

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 875268; // В копейках
+        int bonus = price / 2000;
+        System.out.println(bonus);
+    }
+}
