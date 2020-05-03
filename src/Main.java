526949b8db0fd9fb07dd5be1c636f44ace07cb7c
Mikhail Usachev <63136175+MikhailUsachev-Piter@users.noreply.github.com>
2020-05-03 15:57:52 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        int price = 875268; // В копейках
+        int price = 875268; // Копейки
         int bonus = price / 2000;
         System.out.println(bonus);
     }
